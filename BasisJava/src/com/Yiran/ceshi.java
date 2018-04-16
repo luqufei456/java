ae0507716dc15fba2cbc8bfcb5936da3ad6516ff
yiran <1976967579@qq.com>
2018-04-16 19:20:29 +0800

    哈哈

--- BasisJava/src/com/Yiran/ceshi.java
+++ BasisJava/src/com/Yiran/ceshi.java
@@ -1,4 +1,8 @@
 package com.Yiran;
 
 public class ceshi {
+    public static void main(String[] args){
+        System.out.println("沧海一声笑~");
+        System.out.println("看什么看，没见过这么帅的代码吗？");
+    }
 }
