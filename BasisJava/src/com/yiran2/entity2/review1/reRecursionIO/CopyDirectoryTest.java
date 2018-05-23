e38e4a0fbdfe0d787756ad826a6b96fd0d504e47
yiran <1976967579@qq.com>
2018-05-23 20:08:01 +0800

    文件夹复制思考题

---
+++ BasisJava/src/com/yiran2/entity2/review1/reRecursionIO/CopyDirectoryTest.java
@@ -0,0 +1,21 @@
+package com.yiran2.entity2.review1.reRecursionIO;
+
+/**
+ * IO练习题
+ *
+ * src:源文件夹
+ * dest:目标文件夹
+ * 将src放到dest当中
+ *   步骤:
+ *      1、在目标文件夹中创建新的源文件夹
+ *      2、遍历源文件夹，获取该文件夹下的所有文件对象(包括文件夹与文件)
+ *      3、判断是文件夹还是文件：
+ *          如果是文件夹，就将该文件夹拷贝到目标文件夹中新的源文件夹中(递归过程)
+ *          如果是文件，就将该文件拷贝到目标文件夹中心的源文件夹中
+ * */
+
+public class CopyDirectoryTest {
+    public static void main(String[] args){}
+
+
+}
