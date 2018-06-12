29c756e5a351566fdb883f0ec5bb9f719f0279e4
yiran <1976967579@qq.com>
2018-06-12 15:06:48 +0800

    更新

--- BasisJava/src/com/yiran3/entity2/dbcpDemo/DBCPDemo2.java
+++ BasisJava/src/com/yiran3/entity2/dbcpDemo/DBCPDemo2.java
@@ -27,16 +27,14 @@ public class DBCPDemo2 {
         // 获得类加载器对象
         ClassLoader classLoader = clazz.getClassLoader();
         // 获得资源输入流 这里的路径是相对于src下 而不是项目根目录下
-        InputStream is = classLoader.getResourceAsStream(
-                "dbcp-config.properties");
+        InputStream is = classLoader.getResourceAsStream("dbcp-config.properties");
 
         try {
             /** 第二种方法 */
             // 正常方法获得输入流
             String path = "src\\dbcp-config.properties";
             FileInputStream fis = new FileInputStream(path);
-            Reader reader = new InputStreamReader(fis,
-                    "utf-8");
+            Reader reader = new InputStreamReader(fis, "utf-8");
             // 通过properties加载对象流
             Properties props = new Properties();
             /** 这里不管是is fis 还是reader 都可以 */
@@ -45,8 +43,7 @@ public class DBCPDemo2 {
             // 1.2 获得连接池
             // BasicDataSourceFactory.createDataSource()
             // 通过得到properties的配置来创建一个连接池
-            DataSource dataSource =
-                    BasicDataSourceFactory.createDataSource(props);
+            DataSource dataSource = BasicDataSourceFactory.createDataSource(props);
 
             // 2.获得连接
             Connection conn = dataSource.getConnection();
