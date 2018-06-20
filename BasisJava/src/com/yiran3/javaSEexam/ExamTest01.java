655c646fd6cd8a1ff27786f062f5b8289df4d4c5
yiran <1976967579@qq.com>
2018-06-20 09:16:44 +0800

    JavaSE练习考试

--- BasisJava/src/com/yiran3/javaSEexam/ExamTest01.java
+++ BasisJava/src/com/yiran3/javaSEexam/ExamTest01.java
@@ -1,4 +1,257 @@
 package com.yiran3.javaSEexam;
 
+/**
+ * javaSE 练习考试
+ * */
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class ExamTest01 {
+    @Test
+    public void test01(){
+        /**
+        class A{
+            public int getNumber(int a){
+                return a+1;
+            }
+        }
+        class B extends A {
+            public int getNumber(int a, char c) {
+                return a + 2;
+            }
+
+            public static void main(String[] args) {
+                A a = new B();
+                System.out.println(a.getNumber(0));
+            }
+        }
+         答案为 1 ，参数不同，不能算重写，只能算重载，而且向上转型后，编译看左边，运行看右边。
+         无法构成重写，2个参数的左边没有，只能使用继承的父类的方法，结果为1
+        */
+    }
+
+    @Test
+    public void test02(){
+        /**
+         public void test(){
+             try{
+                 fun();
+                 System.out.print(“情况1”);
+             }catch(ArrayIndexOutOfBoundsException e){
+                 System.out.print(“情况2”);
+             }catch(Exception e){
+                 System.out.print(“情况3”);
+             }finally{
+                 System.out.print(“finally”);
+             }
+         }
+         答案为情况2 finally  数组越界 报错情况2 然后finally必定执行
+         * */
+    }
+
+    @Test
+    public void test03(){
+        /**
+         public static void stringReplace(String text){
+             text=text.replace('j','L');
+         }
+
+         public static void bufferReplace(StringBuffer text){
+             text=text.append("c");
+         }
+
+         public static void main(String args[]){
+             String textString=new String("java");
+             StringBuffer textBuffer=new StringBuffer("java");
+             stringReplace(textString);
+             bufferReplace(textBuffer);
+             System.out.println(textString+textBuffer);
+         }
+
+         答案为 javajavac  方法中的局部变量对外部无影响
+         * */
+    }
+
+    @Test
+    public void test04(){
+        /**
+         Object [] myObjects = {
+             new Integer(12),
+             new String("foo"),
+             new Integer(5),
+             new Boolean(true)
+         };
+         Arrays.sort(myObjects);
+         for( int i=0; i<myObjects.length; i++) {
+             System.out.print(myObjects[i].toString());
+             System.out.print(" ");
+         }
+
+         不同类型无法排序  排序的地方报错，其他没问题
+         * */
+    }
+
+    @Test
+    public void test05(){
+        /**
+           public static void parse(String str) { 
+               try { 
+                   float f= Float.parseFloat(str); 
+               } catch (NumberFormatException nfe) { 
+                   f= 0;        
+               } finally { 
+                   System.out.println(f); 
+               } 
+           } 
+
+           public static void main(String[] args) { 
+               parse("invalid"); 
+           } 
+
+         f不是全局变量，在try中的局部变量，在catch中无法使用，会报错
+         * */
+    }
+
+    @Test
+    public void test06(){
+        int[] x = {122, 33, 55, 678, -987};
+        int a = x[0];
+        for (int i = 1;i < x.length;i++){
+            if (x[i] > a){
+                a = x[i];
+            }
+        }
+        System.out.println(a);
+        // 典型的遍历求最大值
+    }
+
+    @Test
+    public void test07(){
+        /**
+        class Person{
+            public Person(){
+                System.out.println("this is a Person");
+            }
+        }
+        class Teacher extends Person{
+            private String name="tom";
+            public Teacher(){
+                System.out.println("this is a teacher");
+                super();
+            }
+            public static void main(String[] args){
+                Teacher teacher = new Teacher();
+                System.out.println(this.name);
+            }
+        }
+         编译中出现2个错误，super()必须在第一行，主方法中找不到this
+         */
+    }
+
+    @Test
+    public void test08(){
+        /**
+        class Base {
+            abstract public void myfunc();
+            public void another() {
+                System.out.println("Another method");
+            }
+        }
+
+        public class Abs extends Base {
+            public static void main(String[] args) {
+                Abs a = new Abs();
+                a.amethod();
+            }
+
+            public void myfunc() {
+                System.out.println("My func");
+            }
+
+            public void amethod(){
+                myfunc();
+            }
+
+         那么问题来了，一般类里怎么能放抽象方法呢
+            */
+    }
+
+    @Test
+    public void test09(){
+        /**
+        public class Test{
+            static int i;
+
+            public int test(){
+                i++;
+                return i;
+            }
+
+            public static void main(String args[]){
+                Test test=new Test();
+                test.test();
+                System.out.println(test.test());
+            }
+        }
+         int类型初始值为0，这里++两次 就为2
+        */
+    }
+
+    @Test
+    public void test10(){
+        /**
+        class MyClass{
+            public void myMethod (final int p){
+                final int il;
+                final int i2=p;
+                il = 20;
+                i2=20;
+            }
+        }
+         给了初始值的 常量无法再次赋值，所以 i2=20报错
+        */
+    }
+
+    @Test
+    public void test11(){
+        /**
+        class X {
+            Y b = new Y();
+            X() {
+                System.out.print("X");
+            }
+        }
+
+        class Y {
+            Y(){
+                System.out.print("Y");
+            }
+        }
+
+        class Z extends X {
+            Y y = new Y();
+            Z() {
+                // super(); 这里默认调用父类的构造方法
+                System.out.print("Z");
+            }
+
+            public static void main(String[] args) {
+                new Z();
+            }
+        }
+
+         这里要知道子类会调用父类的构造方法 YXYZ
+        */
+    }
+
+    @Test
+    public void test12(){
+        /**
+        a = Integer.parseInt("12");
+        b = Integer.valueOf("12").intValue();
+        intValue()  parseInt()都是返回int类型
+         */
+    }
 }
